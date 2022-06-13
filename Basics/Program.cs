c6233cdf51813802da95ea69a805ff9313d8921a
Murali Krishna <murali1125s@gmail.com>
2022-06-13 09:43:45 +0530

    initial commit

---
+++ Basics/Program.cs
@@ -0,0 +1,109 @@
+﻿
+
+int day = Convert.ToInt32(Console.ReadLine());
+
+switch (day)
+{
+    case 1:
+        Console.WriteLine(" Sunday");
+        break;
+    case 2:
+        Console.WriteLine("monday");
+        break;
+    case 3:
+        Console.WriteLine(" Tuesday");
+        break;
+    case 4:
+        Console.WriteLine(" Wed");
+        break;
+    default:
+        Console.WriteLine("Wrong Input");
+        break;
+}
+//if(day == 1)
+//{
+//    Console.WriteLine("Sunday");
+//}else if (day == 2)
+//{
+//    Console.WriteLine("Monday");
+//}
+//else if (day == 3)
+//{
+//    Console.WriteLine("Tue");
+//}
+//else
+//{
+//    Console.WriteLine(" Wrong Input");
+//}
+//int age = Convert.ToInt16(Console.ReadLine());
+//bool isPh = false;
+//if(age >= 18 && age <= 35)
+//{
+//    Console.WriteLine(" he is eligible");
+//}else if(isPh == false && age >= 18 && age <= 40)
+//{
+//    Console.WriteLine("He is Ph :  he is eligible");
+//}
+//else
+//{
+//    Console.WriteLine(" not Eligible");
+//}
+
+
+
+
+
+//int marks = 240;
+//int newMark = Convert.ToInt16(true);
+//Console.WriteLine(newMark);
+
+
+
+//int age = 18;
+//int marks = 430;
+//Console.WriteLine(" Age of Student is  : "+age);
+//Console.WriteLine("20" + age);
+
+//Console.WriteLine(" enter student marks");
+//marks +=  Convert.ToInt16( Console.ReadLine());
+
+//Console.WriteLine(" Total marks of the student is : " + marks);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// <type> variableName 
+// <type> variableName = value
+// Examples
+/*int age = 43;
+char initial = 'K';
+bool isPresent = false;
+long phone = 9876543210;
+float marksPer = 40.4f;
+double price = 10.50;
+decimal interest = 0.51m;
+short myShort = 1;
+byte myByte = 1;
+
+// Refference type 
+string name = "Rakesh";
+Object myObect = new object(); 
+ */
+
+
---
+++ Basics/ReadMe.txt
@@ -0,0 +1,21 @@
+﻿Variable : is a memoryloaction where data is stored
+	Type :	-> referrence types : address of value where it is
+				-> class, interface, string etc..
+			-> value types : values
+				-> int, bool, long, byte, float, 
+Data Type : it specify the type of the data your going store in a variable 
+	
+Operators : 
+	-> Arthematic : + - % / * 
+	-> Assignment : =, +=, -=, *=, /=, %=
+			int age = 20;
+			age += 4; age => 24
+	-> Logical : && || !
+	-> Unary : ++ --
+	-> ternary : ?:
+	-> Biwise : & | << >> ~ ^
+	-> Relational : > < != == >= <=  
+
+Conditional Statements : 
+	-> if else
+	-> switch
